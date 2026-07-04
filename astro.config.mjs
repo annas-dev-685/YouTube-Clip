495eded92b495b62c2fdde6a2b9cc8ebb486cd97
AnnasBlackHat <annas685@gmail.com>
2026-07-04 20:23:29 +0700

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https:///annas-dev-685.github.io',
+	base: '/YouTube-Clip',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
